bbc0c4bc53da0dfb3a904ecfed0cfd84534c7557
Byadiso <byadiso@gmail.com>
2022-01-02 22:18:11 +0100

    finishing- pluggin

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -14,8 +14,45 @@
         function __construct(){
             add_action('admin_menu', array($this, 'adminPage'));
             add_action('admin_init', array($this, 'settings'));
+            add_filter('the_content', array($this, 'ifWrap'))
         }
 
+        function ifWrap($content){
+            if(is_main_query() AND is_single() AND (get_option('wcp_wordcount','1') OR get_option('wcp_charactercount','1') OR get_option('wcp_readtime','1'))){
+                return $this -> createHTML($content); 
+            }
+            return $content
+        }
+
+        function createHTML($content){
+            $html = '<h3>' . esc_html(get_option('wcp_headline', 'Post statistic')). '</h3><p>';
+
+            //get the word count because both wordcount and read time will need it.
+
+            if(get_option('wcp_wordcount','1') OR 'wcp_readtime','1')){
+                $wordcount = str_word_count(strip_tags($content));
+            }
+    // for word counts
+            if(get_option('wcp_wordcount','1')){
+                $html .='This post has' . $wordcount. ' words. <br>';
+            }
+    // for character count
+            if(get_option('wcp_charactercount','1')){
+                $html .='This post has' . strlen(strip_tags($content)) . ' characters. <br>';
+            }
+
+            // for readtime
+            if(get_option('wcp_readtime','1')){
+                $html .='This post will take about' . round($wordcount / 225) . ' minute(s) to read. <br>';
+            }
+
+            $html .='</p>';
+
+            if(get_option('wcp_location','0') == '0'){
+                return $html . $content;
+            }
+            return $content . $html;
+        }
 
         function settings(){
             add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
@@ -48,11 +85,11 @@
             }
             return $input;
         }
-        
+
         function locationHTML(){ ?>
             <select name="wcp_location">
                 <option value="0" <?php selected(get_option('wcp_location'),'0') ?>>Beginning of post</option>
-                <option value="1" <?php selected(get_option('wcp_location'),'1') ?>>End of post</option>
+                <option value="1" <?php selexcted(get_option('wcp_location'),'1') ?>>End of post</option>
             </select>
 
         <?php }
